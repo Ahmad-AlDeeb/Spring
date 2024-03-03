22ac16a547b002c78f1020245fa416a0e75e78be
Ahmad Al-Deeb <ahmeddeeb22222@gmail.com>
2024-03-03 16:54:35 +0200

    Create Review entity and update Course entity accordingly

--- Chad Darby/demo/17-JPA Mapping/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ Chad Darby/demo/17-JPA Mapping/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -2,13 +2,18 @@ package com.luv2code.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="course")
 public class Course {
+    /** Define fields **/
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private int id;
+
     @Column(name="title")
     private String title;
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
@@ -16,15 +21,19 @@ public class Course {
     @JoinColumn(name="instructor_id")
     private Instructor instructor;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
-    // define constructors
+
+    /** Define constructors **/
     public Course() {}
     public Course(String title) {
         this.title = title;
     }
 
 
-    // define getter setters
+    /** Define getters/setters **/
     public int getId() {
         return id;
     }
@@ -43,9 +52,26 @@ public class Course {
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
     }
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 
 
-    // define toString
+    // Add a convenience method
+    public void addReview(Review theReview) {
+
+        if (reviews == null) {
+            reviews = new ArrayList<>();
+        }
+
+        reviews.add(theReview);
+    }
+
+    // Define toString
     @Override
     public String toString() {
         return "Course{" +
