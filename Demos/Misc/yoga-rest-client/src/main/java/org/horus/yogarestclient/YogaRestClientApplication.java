5a508c9135a079c1cfd9b69417e3ad61811fc83e
Ahmad Al-Deeb <ahmeddeeb22222@gmail.com>
2024-04-21 21:10:40 +0200

    Add yoga-rest-client demo

---
+++ Demos/Misc/yoga-rest-client/src/main/java/org/horus/yogarestclient/YogaRestClientApplication.java
@@ -0,0 +1,13 @@
+package org.horus.yogarestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaRestClientApplication.class, args);
+	}
+
+}
---
+++ Demos/Misc/yoga-rest-client/src/main/java/org/horus/yogarestclient/enums/Role.java
@@ -0,0 +1,6 @@
+package org.horus.yogarestclient.enums;
+
+public enum Role {
+    STUDENT,
+    INSTRUCTOR;
+}
